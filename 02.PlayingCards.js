ef9a323ded09a54284a92aa90b2391f40155e034
Miroslav Tsarev <mtzarev@gmail.com>
2024-11-15 22:41:11 +0200

    Update 02.PlayingCards.js

--- 02.PlayingCards.js
+++ 02.PlayingCards.js
@@ -1,5 +1,5 @@
 function createCard(face, suit){
-    let validFaces = [`2`,`3`,`4`,`5`,`5`,`7`,`8`,`10`,`J`,`Q`,`K`,`A`]
+    let validFaces = [`2`,`3`,`4`,`5`,`5`,`7`,`8`,`10`,`J`,`Q`,`K`,`A`];
     let suitToString = {
         'S': '\u2660',
         'H': '\u2665',
@@ -7,9 +7,9 @@ function createCard(face, suit){
         'C': '\u2663'
     };
     if (validFaces.includes(face)==false){
-        throw new Error(`Invalid face`)
+        throw new Error(`Invalid face`);
     }else if (Object.keys(suitToString).includes(suit)==false){
-        throw new Error(`Invalid suit`)
+        throw new Error(`Invalid suit`);
     }
     
     return { 
@@ -20,4 +20,4 @@ function createCard(face, suit){
         }
     };
 }
-createCard('A', 'D').toString()
+createCard('A', 'D').toString();
